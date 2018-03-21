2b34d275dd13d4a8d2a7a0fcad6721becedc6beb
Jitlada Supreeyaporn <jitlada.su@ku.th>
2018-03-21 11:25:02 +0700

    add link to transaction page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-export default () => <h1>Sawatdee Next.js</h1>
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <h1>Sawatdee Next.js</h1>
+    <p>Index</p>
+    <Link href="/transaction">
+        <a>Transaction Page</a>
+    </Link>
+  </div>
+)
+
+export default Index
