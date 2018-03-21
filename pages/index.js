ed5cea67dc1ef8fee9940c9d6caf6f547daa76f0
Jitlada Supreeyaporn <jitlada.su@ku.th>
2018-03-21 11:50:57 +0700

    use router instead of link

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
-import Link from 'next/link'
+
+import Router from 'next/router'
+
+const handleClickAbout = () => Router.push({
+  pathname: '/transaction'
+})
+
+const style = {
+  backgroundColor: '#4CAF50',
+  border: 'none',
+  color: 'white',
+  padding: '10px 10px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '16px',
+  margin: '4px 2px',
+  cursor: 'pointer',
+}
 
 const Index = () => (
   <div>
+    <a onClick={() => handleClickAbout()} style={style}>About Page</a>
     <h1>Sawatdee Next.js</h1>
     <p>Index</p>
-    <Link href="/transaction">
-        <a>Transaction Page</a>
-    </Link>
   </div>
 )
 
