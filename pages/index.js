218f92871c15e6e18d9ce5311285e55c78ad6c6a
Jitlada Supreeyaporn <jitlada.su@ku.th>
2018-03-21 15:17:13 +0700

    move button to layout and use layout for rendering

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,10 @@
 
-import Router from 'next/router'
-
-const handleClickAbout = () => Router.push({
-  pathname: '/transaction'
-})
-
-const style = {
-  backgroundColor: '#4CAF50',
-  border: 'none',
-  color: 'white',
-  padding: '10px 10px',
-  textAlign: 'center',
-  textDecoration: 'none',
-  display: 'inline-block',
-  fontSize: '16px',
-  margin: '4px 2px',
-  cursor: 'pointer',
-}
+import Layout from '../components/layout'
 
 const Index = () => (
-  <div>
-    <a onClick={() => handleClickAbout()} style={style}>About Page</a>
-    <h1>Sawatdee Next.js</h1>
-    <p>Index</p>
-  </div>
+  <Layout>
+    <h1>Index</h1>
+  </Layout>
 )
 
 export default Index
--- pages/transaction.js
+++ pages/transaction.js
@@ -1,28 +1,10 @@
 
-import Router from 'next/router'
+import Layout from '../components/layout'
 
-const handleClickIndex = () => Router.push({
-  pathname: '/'
-})
-
-const style = {
-  backgroundColor: '#4CAF50',
-  border: 'none',
-  color: 'white',
-  padding: '10px 10px',
-  textAlign: 'center',
-  textDecoration: 'none',
-  display: 'inline-block',
-  fontSize: '16px',
-  margin: '4px 2px',
-  cursor: 'pointer',
-}
-
-const Index = () => (
-  <div>
-    <a onClick={() => handleClickIndex()} style={style}>Index Page</a>
-    <h1>About</h1>
-  </div>
+const Transaction = () => (
+  <Layout>
+    <h1>Transaction</h1>
+  </Layout>
 )
 
-export default Index
+export default Transaction
