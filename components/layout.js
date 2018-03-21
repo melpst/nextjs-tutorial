223422ecc2c3292ea1f5eef613d95382f52712a2
Jitlada Supreeyaporn <jitlada.su@ku.th>
2018-03-21 15:14:35 +0700

    dd components and layout for templating

---
+++ components/layout.js
@@ -0,0 +1,13 @@
+
+import Header from './header'
+import Footer from './footer'
+
+const Layout = (props) => (
+  <div>
+    <Header />
+        {props.children}
+    <Footer />
+  </div>
+)
+
+export default Layout
