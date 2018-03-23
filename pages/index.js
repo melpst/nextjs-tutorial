95ad47ea25d68ca5b6b1dfe413af4fefa33f1a67
Jitlada Supreeyaporn <jitlada.su@ku.th>
2018-03-23 16:38:40 +0700

    add antd template and styles

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import Head from 'next/head'
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
-import Layout from '../components/layout'
-
-const Index = () => (
-  <Layout>
-    <h1>Index</h1>
-  </Layout>
+export default () => (
+  <div>
+    <Head>
+      <link rel='stylesheet' href='https://unpkg.com/antd@3/dist/antd.min.css' />
+    </Head>
+    <Layout>
+    <Sider style={{ background: '#3b3b3b'}}>Sider</Sider>
+    <Layout>
+      <Header style={{ background: '#6b6b6b'}}>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+    </Layout>
+  </div>
 )
-
-export default Index
